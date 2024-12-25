b636e654822ba267b680ddf6bff81c7c5cc23880
Franek-86 <fraleccese86@gmail.com>
2024-12-25 21:34:59 +0100

    test

--- src/pages/OwnedPlants.js
+++ src/pages/OwnedPlants.js
@@ -29,43 +29,52 @@ const OwnedPlants = () => {
           />
         </div>
         <h1 className='section-title'>Le mie piante</h1>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th></th>
-              <th>Data acquisto</th>
-              <th>Tipo</th>
-              <th>Targa</th>
-            </tr>
-          </thead>
-          <tbody>
-            {myPlants &&
-              myPlants.map((plant, index) => {
-                let dateObj = new Date(plant.purchase_date);
-                let myDate = dateObj.toLocaleDateString("it-IT");
-                let date = myDate.split("T")[0];
+        {myPlants ? (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Data acquisto</th>
+                <th>Tipo</th>
+                <th>Targa</th>
+              </tr>
+            </thead>
+            <tbody>
+              {myPlants &&
+                myPlants.map((plant, index) => {
+                  let dateObj = new Date(plant.purchase_date);
+                  let myDate = dateObj.toLocaleDateString("it-IT");
+                  let date = myDate.split("T")[0];
 
-                let [year, month, day] = date.split("-");
-                day = day < 10 ? "0" + day : day;
-                month = month < 10 ? "0" + month : month;
-                let formattedDate = `${day}/${month}/${year}`;
+                  let [year, month, day] = date.split("-");
+                  day = day < 10 ? "0" + day : day;
+                  month = month < 10 ? "0" + month : month;
+                  let formattedDate = `${day}/${month}/${year}`;
 
-                return (
-                  <tr
-                    onClick={() => {
-                      goToPlantPage(plant.id);
-                    }}
-                    key={index}
-                  >
-                    <td>{index + 1}</td>
-                    <td className='bg-info'>{date}</td>
-                    <td className='bg-info'>{plant.plant_type}</td>
-                    <td className='bg-info'>{plant.user_comment}</td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </Table>
+                  return (
+                    <tr
+                      onClick={() => {
+                        goToPlantPage(plant.id);
+                      }}
+                      key={index}
+                    >
+                      <td>{index + 1}</td>
+                      <td className='bg-info'>{date}</td>
+                      <td className='bg-info'>{plant.plant_type}</td>
+                      <td className='bg-info'>{plant.user_comment}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </Table>
+        ) : (
+          <p>
+            Non hai ancora nessuna piantina, puoi scegliere di acquistare una
+            piantina direttamente dalla mappa scegliendo tra le piantine
+            approvate. In alternativa puoi scegliere una piantina approvata
+            dalla lista delle tue segnalazioni.
+          </p>
+        )}
       </div>
     </section>
   );
