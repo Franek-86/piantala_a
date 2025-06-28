5f3abaeb986d69ac1da40bdef2bcb2152bab1e83
Franek-86 <fraleccese86@gmail.com>
2025-06-28 19:53:26 +0200

    test

--- src/services/deepLinkServices.js
+++ src/services/deepLinkServices.js
@@ -4,8 +4,8 @@ export const navigationDeepLink = (navigation) => {
   navigationFunk = navigation;
 };
 
-export const deepLinkFunk = () => {
-  // const token = new URL(url).searchParams("token");
-  // console.log("this is the token from deep link console log", token);
-  navigationDeepLink(`/verification-success`);
+export const deepLinkFunk = (url) => {
+  const token = new URLSearchParams(url).get("token");
+  console.log("this is the token from deep link console log", token);
+  navigationDeepLink(`/verify/${token}`);
 };
