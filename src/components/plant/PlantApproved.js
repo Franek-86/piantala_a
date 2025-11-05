c482911f82efbcd8cb1a5262879a76eb8014dc33
Franek-86 <fraleccese86@gmail.com>
2025-11-05 13:09:54 +0100

    test

--- src/components/plant/PlantApproved.js
+++ src/components/plant/PlantApproved.js
@@ -10,7 +10,9 @@ import Loading from "../../pages/Loading";
 import { copyToClipboard } from "../../utils/utils";
 import PlantForm from "./PlantForm";
 import InfoCard from "./InfoCard";
-
+import { TiLocation } from "react-icons/ti";
+import { BsVectorPen } from "react-icons/bs";
+import { MdPayment } from "react-icons/md";
 const PlantApproved = () => {
   const { plantId } = useParams();
   const navigate = useNavigate();
@@ -42,19 +44,54 @@ const PlantApproved = () => {
               }}
             />
           </div>
-          <h2 className='section-title'>Piantami per amore &#127793;</h2>
-          <p>
-            Verifica qui di seguito le informazioni sulla zona di piantagione
-            tenendo a mente che, li dove non c'è un sistema di irrigazione, sarà
-            tua premura prenderti cura della tua piantina dandogli tutta l'acqua
-            di cui ha bisogno.
-          </p>
-          <p>
-            Potrai successivamente inserire il testo che riporteremo nella targa
-            da apporre sull'albero e proseguire con l'acquisto della tua
-            piantina.
-          </p>
+          <h2 className='section-title mb-5'>Piantami per amore &#127793;</h2>
+          {/* <p>
+            Informazioni sulla zona di piantantagione, inserimento testo targa e
+            procedura di pagamento.
+          </p> */}
+          <section className='steps-section mb-5 d-flex flex-row justify-content-around'>
+            <div className='step-container d-flex flex-column align-items-center'>
+              <div className='step-box d-flex flex-row align-items-center justify-content-center'>
+                <TiLocation />
+              </div>
+              <div className='step-text text-center'>
+                <span className='d-block fw-medium my-1'>Posizione</span>
+                <span className='d-block small'>
+                  Informazioni zona di piantagione
+                </span>
+              </div>
+            </div>
+            <div className='step-container d-flex flex-column align-items-center'>
+              <div className='step-box d-flex flex-row align-items-center justify-content-center'>
+                <BsVectorPen />
+              </div>
+              <div className='step-text text-center'>
+                <span className='d-block fw-medium my-1'>Targa</span>
+                <span className='d-block small'>
+                  Scrivi la tua targa personalizzata
+                </span>
+              </div>
+            </div>
+            <div className='step-container d-flex flex-column align-items-center'>
+              <div className='step-box d-flex flex-row align-items-center justify-content-center'>
+                <MdPayment />
+              </div>
+              <div className='step-text text-center'>
+                <span className='d-block fw-medium my-1'>Pagamento</span>
+                <span className='d-block small'>
+                  Informazioni e procedura pagamento
+                </span>
+              </div>
+            </div>
+          </section>
           <h5 className='mt-4 mb-3'>Zona di piantagione</h5>
+          <p>
+            {" "}
+            Verifica qui di seguito le informazioni su questa zona di
+            piantagione tenendo a mente che, li dove non c'è un sistema di
+            irrigazione, sarà tua premura prenderti cura della tua piantina
+            dandogli tutta l'acqua di cui ha bisogno.
+          </p>
           <InfoCard />
           <h5 className='mt-5 mb-3'>Targa</h5>
           <p>
