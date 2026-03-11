e2f4be4a215ec06d696b43da0f4e24025a63cb8a
Franek-86 <fraleccese86@gmail.com>
2026-03-11 12:32:56 +0100

    test

--- src/context/VersionContext.js
+++ src/context/VersionContext.js
@@ -7,19 +7,24 @@ export const VersionProvider = ({ children }) => {
     process.env.REACT_APP_NODE_ENV === "test"
       ? process.env.REACT_APP_TEST_DOMAIN_NAME_SERVER
       : process.env.REACT_APP_DOMAIN_NAME_SERVER;
+  const appVersion = process.env.REACT_APP_CURRENT_VERSION;
   const version = async () => {
     const checkVersion = async () => {
-      const appVersion = process.env.REACT_APP_CURRENT_VERSION;
-      let response = await fetch(`${serverDomain}/api/version/version-number`);
-      response = await response.json();
-
-      const version = response.message.version_number;
-      console.log("version", version, appVersion);
-      if (version !== appVersion) {
-        window.location.reload();
-        return;
-      } else {
-        return;
+      try {
+        let response = await fetch(
+          `${serverDomain}/api/version/version-number`,
+        );
+        response = await response.json();
+        const version = response.message.version_number;
+        console.log("current version", version, "running version", appVersion);
+        if (version && version !== appVersion) {
+          window.location.reload();
+          return;
+        } else {
+          return;
+        }
+      } catch (err) {
+        console.log(err);
       }
     };
     checkVersion();
