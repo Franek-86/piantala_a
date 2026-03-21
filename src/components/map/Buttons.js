e77c34abba6abb4095357d9f67389baf699c1be0
Franek-86 <fraleccese86@gmail.com>
2026-03-21 11:11:53 +0100

    test

--- src/components/map/Buttons.js
+++ src/components/map/Buttons.js
@@ -128,8 +128,7 @@ const Buttons = ({ setPosition, position, langMatch, latMatch, markerRef }) => {
                 toast.dismiss();
                 toast(piantineAcquistabili, {
                   position: "bottom-right",
-                  // autoClose: 2000,
-                  autoClose: false,
+                  autoClose: 2000,
                   hideProgressBar: false,
                   newestOnTop: true,
                   closeOnClick: false,
