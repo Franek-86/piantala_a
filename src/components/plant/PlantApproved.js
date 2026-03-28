39716692e4e38bbddc4f22cfbd9c9870f0b4cf97
Franek-86 <fraleccese86@gmail.com>
2026-03-28 20:10:15 +0100

    test

--- src/components/plant/PlantApproved.js
+++ src/components/plant/PlantApproved.js
@@ -200,12 +200,12 @@ const PlantApproved = () => {
                     </ul>
                   </span>
                 </div>
-                <div className='d-flex w-100 justify-content-center mt-4'>
+                <div className='d-flex w-100 justify-content-center mt-5'>
                   <Link
                     to={`/map/${plant.id}/location`}
-                    className='btn w-25 btn-success'
+                    className='btn w-50 btn-success'
                   >
-                    Inizia
+                    Inizia il tua acuisto
                   </Link>
                 </div>
               </div>
